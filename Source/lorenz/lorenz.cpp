ed381bbf068446e7a0b7708649db4466193b4879
unpaleness <generalissimusunpaleness@gmail.com>
2019-02-02 15:02:12 +0300

    Draft

---
+++ Source/lorenz/lorenz.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "lorenz.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, lorenz, "lorenz" );
---
+++ Source/lorenz/lorenzGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "lorenzGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LORENZ_API AlorenzGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
